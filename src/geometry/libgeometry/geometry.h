5b69f280ca4216c2879a9ca142c5323f3ccfdf90
Eugenetucha <50362636+Eugenetucha@users.noreply.github.com>
2021-05-02 02:49:25 +0700

    Update geometry.h

--- src/geometry/libgeometry/geometry.h
+++ src/geometry/libgeometry/geometry.h
@@ -1,1 +1,6 @@
+#pragma once
 
+char* Calculations (double x,double y,double r,double perimeter,double area)
+char* proverka_znach(char* point, double* number)
+char* proverka_kruga(char* point)
+  
