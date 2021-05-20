ba81ada387932605ec54f941b0a717e1acf295c1
Eugenetucha <50362636+Eugenetucha@users.noreply.github.com>
2021-05-20 22:15:07 +0700

    Create main.c

---
+++ src/geometry/main.c
@@ -0,0 +1,37 @@
+#include <ctype.h>
+#include <libgeometry/geometry.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct circle_elements {
+    double x;
+    double y;
+    double r;
+} circle;
+
+int main()
+{
+    double perimeter = 0;
+    double area = 0;
+    char figure[100];
+    char* test_point = figure;
+    char* point = figure;
+
+    printf("Vvedite dannye kruga po formatu:");
+    printf("'circle' '(' Point ',' Number ')'\n");
+
+    fgets(figure, sizeof(figure), stdin);
+
+    while (isalpha(*point) != 0)
+        point++;
+
+    if (strncasecmp(test_point, "circle", 6) == 0) {
+        proverka_kruga(point);
+    } else {
+        printf("Excepted 'circle'\n");
+    }
+    printf("Perimeter,%f\n", perimeter);
+    printf("Area,%f\n", area);
+}
