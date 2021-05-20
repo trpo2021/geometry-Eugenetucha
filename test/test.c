60df48cad3bea0aba02f7a7965ee53a462908c66
Eugenetucha <50362636+Eugenetucha@users.noreply.github.com>
2021-05-20 21:41:50 +0700

    Update test.c

--- test/test.c
+++ test/test.c
@@ -1,20 +1,21 @@
-﻿#include <cmath>
 #include <ctest.h>
-#include <iostream>
-#include <libgeometry/libgeometry.h>
-#include <string>
+#include <libgeometry/geometry.h>
+#include <math.h>
+#include <string.h>
 
-CTEST(proverka_kruga,point){
-	char point=(1 1,3);
-	int testExp = 1;
-	int testResult = proverka_kruga(point);
-	ASSERT_EQUAL(testExp, testResult);
+CTEST(proverka_kruga, point)
+{
+    char point = (1 1, 3);
+    int testExp = 1;
+    int testResult = proverka_kruga(point);
+    ASSERT_EQUAL(testExp, testResult);
 }
-CTEST(calculations,znach){
-	double r=3;
-	area=-3;
-	perimeter=-4;
-	int testExp = 1;
-	int testResult = Calculations(x,y,r,perimeter,area);
-	ASSERT_EQUAL(testExp, testResult);
+CTEST(calculations, znach)
+{
+    double r = 3;
+    int area = -3;
+    int perimeter = -4;
+    int testExp = 1;
+    int testResult = Calculations(r);
+    ASSERT_EQUAL(testExp, testResult);
 }
