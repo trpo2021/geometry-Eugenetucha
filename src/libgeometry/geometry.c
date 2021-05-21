4993184d5bc82f880547679034704b3e660a556f
Eugenetucha <50362636+Eugenetucha@users.noreply.github.com>
2021-05-21 23:03:20 +0700

    Update geometry.c

--- src/libgeometry/geometry.c
+++ src/libgeometry/geometry.c
@@ -16,55 +16,54 @@ int Calculations(double r)
 {
     int area = 3.14 * pow(r, 2);
     int perimeter = 2 * 3.14 * r;
-    printf("%d,%d",area,perimeter);
+    printf("%d,%d", area, perimeter);
     return 1;
-}   
-    char* proverka_symv(char* point, char symbol)
-    {
-        while (*point != '\n') {
-            if (*point == symbol) {
-                point++;
-                return point;
-            }
-            if (*point == ' ') {
-                point++;
-            } else {
-                printf("Excepted '%c'\n", symbol);
-                return 0;
+}
+char* proverka_symv(char* point, char symbol)
+{
+    while (*point != '\n') {
+        if (*point == symbol) {
+            point++;
+            return point;
+        }
+        if (*point == ' ') {
+            point++;
+        } else {
+            printf("Excepted '%c'\n", symbol);
+            return 0;
             }
         }
-        return point;
+    return point;
+}
+
+char* proverka_znach(char* point, double* number)
+{
+    char* end;
+    while (isdigit(*point) == 0) {
+        if (*point == ' ') {
+            point++;
+
+        } else {
+            (printf("Excpected one or more elements of circle\n"));
+            return 0;
+        }
     }
 
-            char* proverka_znach(char* point, double* number)
-            {
-                char* end;
-                while (isdigit(*point) == 0) {
-                    if (*point == ' ') {
-                        point++;
-
-                    } else {
-                        (printf("Excpected one or more elements of circle\n"));
-                        return 0;
-                    }
-                }
-
-                if (isdigit(*point) != 0) {
-                    *number = strtod(point, &end);
-                    point = end;
-                    return point;
-                }
-                if (isdigit(*point) > 10.0) {
-                    (printf("Too much value"));
-                    return 0;
-                }
-                if (isdigit(*point) < 0) {
-                    (printf("Use positive values"));
-                    return 0;
-                }
-                return point;
-            }
-
+    if (isdigit(*point) != 0) {
+        *number = strtod(point, &end);
+        point = end;
+        return point;
+    }
+    if (isdigit(*point) > 10.0) {
+        (printf("Too much value"));
+        return 0;
+    }
+    if (isdigit(*point) < 0) {
+        (printf("Use positive values"));
+        return 0;
+    }
+    return point;
+}
 char* proverka_kruga(char* point)
 {
     if ((point = proverka_symv(point, '(')) == 0) {
