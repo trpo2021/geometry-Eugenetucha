6748b4cc374880beb3c98cf1248764a6317e4498
Eugenetucha <50362636+Eugenetucha@users.noreply.github.com>
2021-06-04 16:28:21 +0700

    Update test.c

--- test/test.c
+++ test/test.c
@@ -6,23 +6,23 @@
 CTEST(proverka_kruga, point)
 {
     char point = (1 1, 3);
-    int testExp = 1;
-    int testResult = proverka_kruga(point);
+    char testExp = 1;
+    char testResult = proverka_kruga(point);
     ASSERT_EQUAL(testExp, testResult);
 }
 CTEST(calculations1, znach)
 {
-    int r = 3;
-    int area = -3;
-    int testExp >= 1;
-    int testResult = Calculations1(r);
+    char r = 3;
+    char area = -3;
+    char testExp >= 1;
+    char testResult = Calculations1(r);
     ASSERT_EQUAL(testExp, testResult);
 }
 CTEST(calculations2, znach)
 {
-    int r = 3;
-    int perimeter = -4;
-    int testExp >= 1;
-    int testResult = Calculations2(r);
+    char r = 3;
+    char perimeter = -4;
+    char testExp >= 1;
+    char testResult = Calculations2(r);
     ASSERT_EQUAL(testExp, testResult);
 }
